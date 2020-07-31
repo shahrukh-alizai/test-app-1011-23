50aab1af71049e7a0c73d8916a29b9082e0722b1
Shah Rukh Khan <shah@crowdbotics.com>
2020-07-31 14:18:34 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -8,6 +8,13 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    asfd = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="customtext_asfd",
+    )
 
     def __str__(self):
         return self.title
